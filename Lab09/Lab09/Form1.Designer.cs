9b2f141dd1be54a8a5cd06821f59fdf310ef8604
geartide <80594346+geartide@users.noreply.github.com>
2022-05-28 21:34:27 +0300

    Сделать работу

---
+++ Lab09/Lab09/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace Lab09
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBoxCLOCK = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCLOCK)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // pictureBoxCLOCK
+            // 
+            this.pictureBoxCLOCK.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxCLOCK.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBoxCLOCK.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxCLOCK.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBoxCLOCK.Name = "pictureBoxCLOCK";
+            this.pictureBoxCLOCK.Size = new System.Drawing.Size(964, 645);
+            this.pictureBoxCLOCK.TabIndex = 0;
+            this.pictureBoxCLOCK.TabStop = false;
+            this.pictureBoxCLOCK.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBoxCLOCK_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(963, 644);
+            this.Controls.Add(this.pictureBoxCLOCK);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCLOCK)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.PictureBox pictureBoxCLOCK;
+    }
+}
+
---
+++ Lab09/Lab09/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing.Drawing2D;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lab09
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBoxCLOCK_Paint(object sender, PaintEventArgs e)
+        {
+            pictureBoxCLOCK.BackColor = Color.FromArgb(15, 0, 76, 154);
+            Graphics graphics = e.Graphics;
+            GraphicsState gs;
+
+            int width = this.Width;
+            int height = this.Height;
+
+            DateTime dateTime = DateTime.Now;
+            graphics.TranslateTransform(width / 2, height / 2);
+
+            graphics.RotateTransform(225);
+
+            gs = graphics.Save();
+            graphics.RotateTransform(6 * dateTime.Second);
+
+            graphics.DrawLine(new Pen(Color.Black, 1), 0, 0, 50, 50);
+            graphics.FillEllipse(new SolidBrush(Color.Red), 50, 50, 8, 8);
+
+            graphics.Restore(gs);
+
+            gs = graphics.Save();
+            graphics.RotateTransform(6 * dateTime.Minute + dateTime.Second / 10);
+
+            PointF[] MinutePts =
+            {
+                new PointF(8.0F, 0.0F),
+                new PointF(55.0F, 55.0F),
+                new PointF(-8.0F, 0.0F),
+                new PointF(5.0F, 5.0F)
+            };
+
+            graphics.FillPolygon(new SolidBrush(Color.Black), MinutePts);
+            graphics.Restore(gs);
+
+            gs = graphics.Save();
+            graphics.RotateTransform(30 * dateTime.Hour + (float)dateTime.Minute / 2);
+
+            PointF[] HourPts =
+            {
+                new PointF(5.0F, -2.0F),
+                new PointF(50.0F, 50.0F),
+                new PointF(-5.0F, 2.0F),
+                new PointF(7F, 7F)
+            };
+
+            graphics.FillPolygon(new SolidBrush(Color.Black), HourPts);
+            graphics.Restore(gs);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            GraphicsState graphicsState;
+            int width = this.Width;
+            int height = this.Height;
+
+            graphics.TranslateTransform(width / 2, height / 2);
+            Pen black_pen = new Pen(Color.Black, 3);
+            Pen red_pen = new Pen(Color.Red, 3);
+
+            graphics.FillEllipse(new SolidBrush(Color.White), -105, -105, 210, 210);
+            graphics.DrawEllipse(black_pen, -105, -105, 210, 210);
+
+            for (int i = 0; i < 12; i++)
+            {
+                graphicsState = graphics.Save();
+                graphics.RotateTransform(30 * i + 45);
+                graphics.DrawLine(red_pen, -57, -57, -70, -70);
+                graphics.Restore(graphicsState);
+            }
+
+            for (int i = 0; i < 60; i++)
+            {
+                graphicsState = graphics.Save();
+                graphics.RotateTransform(6 * i + 45);
+                graphics.DrawLine(black_pen, -63, -63, -70, -70);
+                graphics.Restore(graphicsState);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer.Enabled = true;
+            this.Invalidate();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            pictureBoxCLOCK.Invalidate();
+        }
+    }
+}
